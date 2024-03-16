a5f5723cabf2312c7483757cf6873b5aaddff7f4
pocketful <iveta.jac@gmail.com>
2024-03-16 04:22:26 +0200

    PhotoCard responsive images, imageLoadError

--- src/components/GalleryPage/PhotoCard/PhotoCard.tsx
+++ src/components/GalleryPage/PhotoCard/PhotoCard.tsx
@@ -1,6 +1,7 @@
 import FavouriteButton from '../FavouriteButton/FavouriteButton'
 import style from './PhotoCard.module.scss'
 import { extractDescriptionFromUrl } from '../utils'
+import { useState } from 'react'
 import { PhotoModel } from '../../../types/models/photo'
 
 type Props = {
@@ -17,6 +18,19 @@ const PhotoCard = ({
   onToggleFavourite,
   isFavourite,
 }: PhotoModel & Props) => {
+  const [imageLoadError, setImageLoadError] = useState(false)
+
+  const handleImageError = () => {
+    setImageLoadError(true)
+  }
+
+  const srcSet = `${src.medium} 350w, ${src.large} 940w`
+  const sizes = `(max-width: 350px) calc((100vw - 20px - 10px) / 2),
+                 (max-width: 576px) calc((100vw - 30px - 15px) / 2),
+                 (max-width: 768px) calc((100vw - 40px - 40px) / 3),
+                 (max-width: 1200px) calc((100vw - 60px - 50px) / 3),
+                 calc((100vw - 100px - 100px) / 3)`
+
   return (
     <article className={style.card}>
       <div className={style.content}>
@@ -28,15 +42,19 @@ const PhotoCard = ({
           Favourite
         </FavouriteButton>
       </div>
-
-      <img
-        loading="lazy"
-        src={src.medium}
-        className={style.photo}
-        alt={alt || `Photo by ${photographer}`}
-      ></img>
-
-      <p className={style.error}>Image failed to load.</p>
+      {!imageLoadError ? (
+        <img
+          loading="lazy"
+          src={src.medium}
+          srcSet={srcSet}
+          sizes={sizes}
+          className={style.photo}
+          alt={alt || `Photo by ${photographer}`}
+          onError={handleImageError}
+        ></img>
+      ) : (
+        <p className={style.error}>Image failed to load.</p>
+      )}
     </article>
   )
 }
